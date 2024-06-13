40fcc4e191a4ecb18221ae9a840acb5cb01994d8
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-06-13 19:33:48 +0200

    tâches rendues dynamique

--- src/components/TodosComponent.vue
+++ src/components/TodosComponent.vue
@@ -11,24 +11,13 @@
         <label for="toggle-all-input" class="toggle-all-label">Tout afficher</label>
       </div>
       <ul class="todo-list">
-        <li>
+        <li v-for="todo in todos" :key="todo.id">
           <div class="view">
-            <input type="checkbox" id="" class="toggle" />
-            <label for="">Tâche 1</label>
+            <input type="checkbox" :id="todo.id" class="toggle" />
+            <label :for="todo.id">{{  todo.title }}</label>
           </div>
         </li>
-        <li>
-          <div class="view">
-            <input type="checkbox" id="" class="toggle" />
-            <label for="">Tâche 2</label>
-          </div>
-        </li>
-        <li>
-          <div class="view">
-            <input type="checkbox" id="" class="toggle" />
-            <label for="">Tâche 3</label>
-          </div>
-        </li>
+        
       </ul>
     </main>
     <!-- Liste des tâches -->
@@ -38,7 +27,18 @@
 </template>
 
 <script setup lang="ts">
-import TodoHeader from '@/components/TodoHeader.vue'
-import TodoFooter from '@/components/TodoFooter.vue'
+import { ref } from 'vue';
+import TodoHeader from '@/components/TodoHeader.vue';
+import TodoFooter from '@/components/TodoFooter.vue';
+
+
+const todos = ref([
+    {id: "888e49c6-39e3-4ae5-88fa-582e3371c96e", title: "Ma tâche 1", completed: false},
+    {id: "bd6673fd-ff73-4f6e-9552-1e009cbfbd3d", title: "Ma tâche 2", completed: false},
+    {id: "6615b6d1-9e66-4843-8b8c-f0125e74aa66", title: "Ma tâche 3", completed: false},
+]);
+
+
+
 </script>
 <style></style>
