a7f12545b1dd288ed1ea741c8656caf1e7d343c8
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-06-13 18:59:48 +0200

    footer créé

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>Footer</div>
+  <footer class="footer">
+    <span class="todo-count"> <strong>10</strong> tâches restantes </span>
+
+    <ul class="filters">
+      <li>
+        <RouterLink to="#">Tous</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="#">En cours</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="#">Terminé</RouterLink>
+      </li>
+
+      <button class="clear-completed">Sup. tâches terminées</button>
+    </ul>
+  </footer>
 </template>
 
 <script setup lang="ts"></script>
