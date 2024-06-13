67b07813f932d79eddd5d4b6a96a6e7eade65ece
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-06-13 20:27:58 +0200

    ajout de la fonction addTodo

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -7,10 +7,17 @@
       placeholder="Entrez une tâche"
       autofocus
       autocomplete="off"
+      @keyup.enter="emit('add-toto', ($event.target as HTMLInputElement).value)"
     />
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+// const emit = defineEmits(["add-toto"]);
+const emit = defineEmits<{
+    (e: 'add-toto', value: string): void
+}>();
+</script>
 
 <style scoped></style>
--- src/components/TodosComponent.vue
+++ src/components/TodosComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- En-tête (titre, input) -->
-    <TodoHeader />
+    <TodoHeader @add-toto="addTodo"/>
     <!-- En-tête (titre, input) -->
 
     <!-- Liste des tâches -->
@@ -29,12 +29,21 @@
 import { ref } from 'vue'
 import TodoHeader from '@/components/TodoHeader.vue'
 import TodoFooter from '@/components/TodoFooter.vue'
-import type { Todo } from '@/@types'
+import type { Todo } from '@/@types';
+import { nanoid } from 'nanoid';
 
 const todos = ref<Todo[]>([
   { id: '888e49c6-39e3-4ae5-88fa-582e3371c96e', title: 'Ma tâche 1', completed: false },
   { id: 'bd6673fd-ff73-4f6e-9552-1e009cbfbd3d', title: 'Ma tâche 2', completed: false },
   { id: '6615b6d1-9e66-4843-8b8c-f0125e74aa66', title: 'Ma tâche 3', completed: false }
 ])
+
+function addTodo(value: string){
+   todos.value.push({
+       id: nanoid(),
+       title: value,
+       completed: false
+   })
+}
 </script>
 <style></style>
